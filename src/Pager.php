3b0a1ca393885f2949e7cd5107a55cfbae0323c8
S.M. Ataur Rahman <dev.ataur.rahman@gmail.com>
2020-10-09 13:19:10 +0600

    Pagination is now marked as final class && Pager is now uses composition

--- src/Pager.php
+++ src/Pager.php
@@ -3,17 +3,59 @@ declare(strict_types=1);
 
 namespace Falgun\Pagination;
 
-class Pager extends Pagination
+final class Pager implements PaginationInterface
 {
+
+    private Pagination $pagination;
+
+    public function __construct(int $currentPage, int $itemsPerPage = 10)
+    {
+        $this->pagination = new Pagination($currentPage, $itemsPerPage, 0);
+    }
 
     public function make(): PaginationBag
     {
         return PaginationBag::new(
                 Page::new('', 1, false, Page::NO_FLAG),
                 Page::new('', 1, false, Page::NO_FLAG),
-                PageBuilder::prePage($this->currentPage),
-                PageBuilder::nextPage($this->currentPage, $this->totalPage),
+                PageBuilder::prePage($this->getCurrentPage()),
+                PageBuilder::nextPage($this->getCurrentPage(), $this->getTotalPage()),
                 new \EmptyIterator()
         );
+    }
+
+    public function getCurrentPage(): int
+    {
+        return $this->pagination->getCurrentPage();
+    }
+
+    public function getItemOffset(): int
+    {
+        return $this->pagination->getItemOffset();
+    }
+
+    public function getItemsPerPage(): int
+    {
+        return $this->pagination->getItemsPerPage();
+    }
+
+    public function getTotalItems(): int
+    {
+        return $this->pagination->getTotalItems();
+    }
+
+    public function getTotalPage(): int
+    {
+        return $this->pagination->getTotalPage();
+    }
+
+    public function hasMultiplePage(): bool
+    {
+        return $this->pagination->hasMultiplePage();
+    }
+
+    public function setTotalItems(int $total): PaginationInterface
+    {
+        return $this->pagination->setTotalItems($total);
     }
 }
--- src/Pagination.php
+++ src/Pagination.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Falgun\Pagination;
 
-class Pagination implements PaginationInterface
+final class Pagination implements PaginationInterface
 {
 
     protected int $itemOffset;
